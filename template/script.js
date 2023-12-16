f18fc2f56dbea6f24f509fc7fc3e8ffd70dad9b3
KainTheDev <thica.doi@gmail.com>
2023-12-16 18:28:00 +0700

    .

--- template/script.js
+++ template/script.js
@@ -1,4 +1,6 @@
-const modal = document.getElementById('Hello_modal')
-function showModal(boolean) {
-
+async function setModal(id, visibility, padding, callback) {
+    const modal = document.getElementById(id)
+    modal.style.visibility = visibility
+    modal.style.padding = padding
+    if(typeof callback === 'function') await callback(modal)
 }
