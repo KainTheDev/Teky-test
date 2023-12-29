6b3c6b927b4698abd72695290fe3e0f754d3ffae
Kain <89621266+KainTheDev@users.noreply.github.com>
2023-12-29 13:04:55 +0000

    .

--- template/script.js
+++ template/script.js
@@ -1,6 +1,21 @@
-async function setModal(id, visibility, padding, callback) {
+const productions = [{
+    title: 'multi-purpose API',
+    linkName: 'google.com',
+    link: 'https://google.com'
+},
+{
+    title: 'multi-purpose bot',
+    linkName: 'google.com',
+    link: 'https://google.com'
+}]
+const closeButton = document.getElementById('close-button')
+
+function setModal(id, visibility, padding, callback) {
     const modal = document.getElementById(id)
+    modal.innerHTML = productions
+    .map(data => `${data.title}: <button class="hyperlink" onclick="window.open('${data.link}', '_blank')">${data.linkName}</button><br>`).join('')
+    closeButton.style.visibility = visibility
     modal.style.visibility = visibility
     modal.style.padding = padding
-    if(typeof callback === 'function') await callback(modal)
+    if(typeof callback === 'function') callback(modal)
 }
